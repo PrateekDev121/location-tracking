1bbda582048dd47b6b9011776cc733d3ed32a366
Prateek <prateek.dev121@gmail.com>
2025-05-20 06:59:50 +0530

    -add TYPESAFE_PROJECT_ACCESSORS feature

--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -40,4 +40,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //module dependencies
+    implementation(projects.auth.domain)
+    implementation(projects.core.domain)
+    implementation(projects.core.data)
 }
